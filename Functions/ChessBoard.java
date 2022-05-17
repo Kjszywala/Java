e2822f9499990f99c126ae1d2cdb0fb9c44358fe
Kamil Szywala <kjszywala@protonmail.com>
2022-05-17 13:39:54 +0100

    Update ChessBoard.java

--- Functions/ChessBoard.java
+++ Functions/ChessBoard.java
@@ -1,4 +1,5 @@
-public class ChessBoard extends JComponent{
+public class ChessBoard extends JComponent {
+    
     private final int HEIGHT = 100;
     private final int WIDTH = 100;
     protected int positionX = 0;
@@ -16,10 +17,10 @@ public class ChessBoard extends JComponent{
     };
     
     @Override
-    public void paintComponent(Graphics g){
-        for(int j = 0; j<8; j++){
-            for(int i = 0; i<8; i++){
-                if(isWhite){
+    public void paintComponent(Graphics g) {
+        for(int j = 0; j<8; j++) {
+            for(int i = 0; i<8; i++) {
+                if(isWhite) {
                     g.setColor(Color.white);
                     g.fillRect(i*100, j*100, WIDTH, HEIGHT);
                 } else {
@@ -30,15 +31,13 @@ public class ChessBoard extends JComponent{
             }
             isWhite = !isWhite;
         }
-        for(int i = 0; i < 8; i++){
-            for(int j = 0; j < 8; j++){
-                if(pawns[j][i] == 2)
-                {
+        for(int i = 0; i < 8; i++) {
+            for(int j = 0; j < 8; j++) {
+                if(pawns[j][i] == 2) {
                     g.setColor(Color.decode("#803f1f"));
                     g.fillOval(5 + (i * 100), 5 + (j * 100), size, size);
                 }
-                else if(pawns[j][i] == 1)
-                {
+                else if(pawns[j][i] == 1) {
                     g.setColor(Color.decode("#e6deda"));
                     g.fillOval(5 + (i * 100), 5 + (j * 100), size, size);
                 }
